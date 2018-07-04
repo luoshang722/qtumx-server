87c4a287f82726724f88ca4d49862897bfe28c01
Qi Weiyu <qi@weiyu.me>
2018-07-04 14:42:34 +0800

    init

---
+++ db/db.sql
@@ -0,0 +1,66 @@
+-- MySQL dump 10.13  Distrib 5.7.20, for osx10.12 (x86_64)
+--
+-- Host: localhost    Database: qtumx
+-- ------------------------------------------------------
+-- Server version	5.7.20
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `chain`
+--
+
+DROP TABLE IF EXISTS `chain`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `chain` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `chainId` varchar(1024) NOT NULL,
+  `desc` varchar(1024) NOT NULL,
+  `msgHeader` char(8) NOT NULL,
+  `defaultPort` int(10) unsigned NOT NULL,
+  `detail` text,
+  `createdBy` varchar(128) NOT NULL,
+  `createdAt` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `i_chainId` (`chainId`(50)),
+  KEY `i_desc` (`desc`(50)),
+  KEY `i_msgHeader` (`msgHeader`),
+  KEY `i_defaultPort` (`defaultPort`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `session_data`
+--
+
+DROP TABLE IF EXISTS `session_data`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `session_data` (
+  `id` varchar(64) NOT NULL,
+  `token` char(64) NOT NULL DEFAULT '',
+  `expired` bigint(20) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-07-04 14:40:39
